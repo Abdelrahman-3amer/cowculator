a19d707ca943c34588113aac25388c0011de995e
Abdelrahman-3amer <abdelrahmanamer252@gmail.com>
2024-07-31 13:33:10 +0300

    Initial Commit

---
+++ FinalProjectBM/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  FinalProjectBM
+//
+//  Created by Abdelrahman Amer on 31/07/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
